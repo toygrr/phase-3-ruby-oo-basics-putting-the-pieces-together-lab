bd77bdd7abfb91adf4b5ae5aefb25e0e39ddffcb
Tyler <toygrrr@gmail.com>
2022-08-30 10:41:28 -0700

    done

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,47 @@
 
 
+# Make your shoe class here!
+
+class Book
+attr_accessor :title, :author, :page_count, :genre
+    def initialize(title)
+        @title = title
+    end
+
+    def title=(title)
+        @title = title
+    end
+
+    def title
+        @title
+    end
+
+    def author=(author)
+        @author = author
+    end
+
+    def author
+        @author
+    end
+
+    def page_count
+        @page_count = page_count
+    end
+
+    def page_count 
+        @page_count
+    end
+
+    def genre
+        @genre = genre
+    end
+
+    def genre
+        @genre
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+end
